243a02c766c4422a9115cf9f2729af6510359f1a
mahmoud <mahmoud@coursera-something.com>
2022-07-09 17:07:11 +0200

    project from local

---
+++ src/com/mahmoud/secondapp/package1/MainClass.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package com.mahmoud.secondapp.package1;
+
+import java.util.Scanner;
+
+
+/**
+ * @author mahmoud
+ *
+ */
+public class MainClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("welcom Aboadel");
+		
+		
+//		Scanner s = new Scanner(System.in);
+		
+//		System.out.print(s);
+		int x ,y, z ;
+		x = 10;
+		
+		
+
+	}
+
+}
